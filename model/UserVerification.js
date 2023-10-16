a10d8f35347c2bcddd0493ae2da84003f07a9eb0
Rahman Ramees <rhmnramees730@gmail.com>
2023-10-16 16:21:10 +0530

    Email Verification

---
+++ model/UserVerification.js
@@ -0,0 +1,14 @@
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema;
+
+// const UserVerificationSchema = new Schema({
+//     userId:String,
+//     uniqueString:String,
+//     createdAt:Date,
+//     expiresAt:Date,
+//     verified:Boolean
+// })
+
+// const UserVerification = mongoose.model('UserVerification', UserVerificationSchema);
+
+// model.exports = UserVerification
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express();
+
+const port = 3000;
+
+// mongodb 
+require('./config/db')
+
+// for accepting post form data
+const bodyparser = require('express').json;
+app.use(bodyparser());
+
+
+
+const userRouter = require('./api/user')
+
+// const bodyParser = require('body-parser');
+
+app.use('/user',userRouter)
+
+app.use(express.json());
+
+
+app.listen(port,() => {
+    console.log(`server running on port ${port}`);
+} )
+
+
+
